ba5183f16780912559934a3b81b6d710866dbf73
Prashant <prashantkashyap2707@gmail.com>
2024-03-03 09:41:47 +0530

    last

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { Navigation } from 'swiper/modules';
 import SwiperCore from 'swiper';
 import 'swiper/css/bundle';
 import ListingItem from '../components/ListingItem';
-import backImage from "../"
+// import backImage from "../"
 
 export default function Home() {
   const [offerListings, setOfferListings] = useState([]);
@@ -49,7 +49,7 @@ export default function Home() {
   return (
     <div>
       {/* top */}
-      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto bg-[url(".../background.jpg")]'>
+      <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto '>
         <h1 className='text-slate-700 font-bold text-3xl lg:text-6xl'>
           Find your next <span className='text-slate-500'>perfect</span>
           <br />
